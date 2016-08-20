02f8ca534e49a0b29b7dd6a7c2c44e3f246cbf20
cclauss <cclauss@bluewin.ch>
2016-08-20 12:03:15 +0200

    Add a Python 3 fork of Fabric

--- pypi_parser.py
+++ pypi_parser.py
@@ -28,6 +28,7 @@ EQUIVALENTS = {
     'argparse': 'https://docs.python.org/3/library/argparse.html',
     'BeautifulSoup': 'http://pypi.python.org/pypi/beautifulsoup4/',
     'dnspython': 'http://pypi.python.org/pypi/dnspython3',
+    'Fabric': 'https://github.com/mathiasertl/fabric',
     'functools32': 'https://docs.python.org/3/library/functools.html',
     'futures': 'http://docs.python.org/3/library/concurrent.futures.html',
     'ipaddr': 'https://docs.python.org/3/library/ipaddress.html',
